fcc5858a270fe1ec191aff2d0ac25b75df910039
CountrySideEngineer <deep-sleeper@nifty.com>
2025-07-15 08:01:39 +0900

    Add view model base class.

---
+++ dev/src/ProgresssDialog.Wpf/ViewModel/ViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgresssDialog.Wpf.ViewModel
+{
+    internal class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public virtual void RaisePropertyChange([CallerMemberName ]string proName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(proName));
+        }
+    }
+}
