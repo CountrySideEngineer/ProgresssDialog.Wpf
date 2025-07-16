98ff91b32a665117a4de61f322d466bee4219a2b
CountrySideEngineer <deep-sleeper@nifty.com>
2025-07-16 22:44:08 +0900

    Add ProgressItemViewModel class.

---
+++ dev/src/ProgresssDialog.Wpf/ViewModel/ProgressItemViewModel.cs
@@ -0,0 +1,89 @@
+﻿using ProgresssDialog.Wpf.Model.Args;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgresssDialog.Wpf.ViewModel
+{
+    /// <summary>
+    /// ViewModel for representing the progress of a single process item in the UI.
+    /// </summary>
+    internal class ProgressItemViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Backing field for the process name.
+        /// </summary>
+        protected string _processName = string.Empty;
+
+        /// <summary>
+        /// Backing field for the numerator (current progress value).
+        /// </summary>
+        protected int _numerator = 0;
+
+        /// <summary>
+        /// Backing field for the denominator (maximum progress value).
+        /// </summary>
+        protected int _denominator = 0;
+
+        /// <summary>
+        /// Gets or sets the name of the process.
+        /// </summary>
+        public string ProcessName
+        {
+            get => _processName;
+            set
+            {
+                _processName = value;
+                RaisePropertyChange();
+            }
+        }
+
+        /// <summary>
+        /// Gets the progress percentage (0-100). Returns 0 if denominator is 0.
+        /// </summary>
+        public int Progress => (0 != _denominator) ? ((_numerator * 100) / _denominator) : 0;
+
+        /// <summary>
+        /// Gets or sets the current progress value (numerator).
+        /// </summary>
+        public int Numerator
+        {
+            get => _numerator;
+            set
+            {
+                _numerator = value;
+                RaisePropertyChange();
+                RaisePropertyChange(nameof(Progress));
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the maximum progress value (denominator).
+        /// </summary>
+        public int Denominator
+        {
+            get => _denominator;
+            set
+            {
+                _denominator = 0 == value ? 0 : value;
+                RaisePropertyChange();
+                RaisePropertyChange(nameof(Progress));
+            }
+        }
+
+        /// <summary>
+        /// Handles progress changed events and updates the ViewModel properties accordingly.
+        /// </summary>
+        /// <param name="sender">The event source.</param>
+        /// <param name="e">The event arguments containing progress information.</param>
+        public virtual void OnProgressChanged(object sender, ProgressChangedEventArg e)
+        {
+            ProcessName = e.ProgressInfo.ProcessName;
+            Denominator = e.ProgressInfo.Denominator;
+            Numerator = e.ProgressInfo.Numerator;
+        }
+    }
+}
