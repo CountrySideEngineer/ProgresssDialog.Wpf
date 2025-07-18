c72e6b4f928ee4159e7ba88ade82dbf737cc1bd8
CountrySideEngineer <deep-sleeper@nifty.com>
2025-07-19 07:13:54 +0900

    Update ProgressWindowView class. Implement event handler. Add code behind.

--- dev/src/ProgresssDialog.Wpf/View/ProgressWindowView.xaml.cs
+++ dev/src/ProgresssDialog.Wpf/View/ProgressWindowView.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ProgresssDialog.Wpf.ViewModel;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace ProgresssDialog.Wpf.View
 {
@@ -19,9 +9,63 @@ namespace ProgresssDialog.Wpf.View
     /// </summary>
     public partial class ProgressWindowView : Window
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProgressWindowView"/> class.
+        /// </summary>
         public ProgressWindowView()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles the DataContextChanged event of the window.
+        /// Subscribes to the CloseRequested event of the <see cref="ProgressWindowViewModel"/> to close the window when requested.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The event data.</param>
+        private void Window_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if ((e.OldValue != null) && (e.OldValue is ProgressWindowViewModel oldViewModel))
+            {
+                oldViewModel.CloseRequested -= OnWindowClose;
+            }
+            if (e.NewValue is ProgressWindowViewModel viewModel)
+            {
+                viewModel.CloseRequested += OnWindowClose;
+            }
+        }
+
+        /// <summary>
+        /// Handles the CloseRequested event from the ViewModel and closes the window.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The event data.</param>
+        private void OnWindowClose(object? sender, EventArgs e)
+        {
+            // Ensure Close is called on the UI thread
+            if (!Dispatcher.CheckAccess())
+            {
+                Dispatcher.Invoke(Close);
+            }
+            else
+            {
+                Close();
+            }
+        }
+
+        /// <summary>
+        /// Handles the ContentRendered event of the window.
+        /// Invokes the <see cref="ProgressWindowViewModel.OnProgressRequested"/> method on the DataContext
+        /// to start the progress operation when the window content is first rendered.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The event data.</param>
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            if (DataContext is ProgressWindowViewModel viewModel)
+            {
+                viewModel.OnProgressRequested(this, EventArgs.Empty);
+            }
         }
     }
 }
